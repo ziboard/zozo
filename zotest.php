9ba636e1b33f453684915e65bdd526e552416ff2
ziboard <djzoheir@gmail.com>
2016-12-30 16:33:28 +0100

    update php

--- zotest.php
+++ zotest.php
@@ -1,9 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>php file_put_contents() function - how to append a string to a file in php</title>
+</head>
+
+<body>
+    <h2 style="color:Crimson; font-style:italic">php file_put_contents() function example:<br /> how to append a string to a file</h2>
+    <hr width="375" align="left" color="Orange" />
+    <br />
 
 <?php
-$filename = 'files/test.txt';
-$str = 'Free PHP course and tutorials.'. PHP_EOL. 'Web site: http://coursesweb.net';
 
-// creates the 'text.txt' file in the 'files' folder
-if (file_put_contents($filename, $str)) echo 'The file was created';
-else echo 'The test.txt file can not be created';
+ $file = "zo123.txt";
+ echo  file_put_contents($file," hello! ",FILE_APPEND);
+ 
 ?>
+</body>
+</html>
