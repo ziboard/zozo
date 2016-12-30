ab60afbda8b7d02ca1a8e1fc36bb5a828488c776
ziboard <djzoheir@gmail.com>
2016-12-30 16:28:50 +0100

    update php

--- zotest.php
+++ zotest.php
@@ -1,11 +1,9 @@
 
 <?php
-$filename = 'zo123.txt';
+$filename = 'files/test.txt';
 $str = 'Free PHP course and tutorials.'. PHP_EOL. 'Web site: http://coursesweb.net';
 
-
-file_put_contents($filename,"zo txt ok",FILE_APPEND);
-
+// creates the 'text.txt' file in the 'files' folder
+if (file_put_contents($filename, $str)) echo 'The file was created';
+else echo 'The test.txt file can not be created';
 ?>
-
-
