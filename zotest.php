01a48ea3f0fad4ba6e5ee776fdca8b8cb54eb89d
ziboard <djzoheir@gmail.com>
2016-12-30 16:22:00 +0100

    zo php

---
+++ zotest.php
@@ -0,0 +1,11 @@
+
+<?php
+$filename = 'zo123.txt';
+$str = 'Free PHP course and tutorials.'. PHP_EOL. 'Web site: http://coursesweb.net';
+
+
+file_put_contents($filename,"zo txt ok",FILE_APPEND);
+
+?>
+
+
